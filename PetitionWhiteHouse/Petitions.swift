d111b05893ef9bd5c53661d28796bb5362ac68c9
AndresGutierrez <c0dingw.dre@gmail.com>
2022-01-29 14:13:34 -0800

    setup model for json create url and parse json data

---
+++ PetitionWhiteHouse/Petitions.swift
@@ -0,0 +1,18 @@
+//
+//  Petitions.swift
+//  PetitionWhiteHouse
+//
+//  Created by Andres Gutierrez on 1/28/22.
+//
+
+import Foundation
+
+struct MetaData: Codable {
+    var results = [Petitions]()
+}
+
+struct Petitions: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
--- PetitionWhiteHouse/ViewController.swift
+++ PetitionWhiteHouse/ViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [String]()
+    var petitions   = [Petitions]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url){
+                parseJson(json: data)
+            }
+        }
     }
+    
+    
+    func parseJson(json: Data){
+        let decoder = JSONDecoder()
+        
+        if let jsonDecoded  = try? decoder.decode(MetaData.self, from: json){
+            petitions   = jsonDecoded.results
+        }
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
 
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell    = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text    = "Title goes here"
-        cell.detailTextLabel?.text  = "Subtitle goes here"
+        let petition                = petitions[indexPath.row]
+        cell.textLabel?.text        = petition.title
+        cell.detailTextLabel?.text  = petition.body
         return cell
     }
 }
