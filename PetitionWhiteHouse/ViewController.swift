de9a475560bf08360299299105c5c79353cbcaf7
AndresGutierrez <c0dingw.dre@gmail.com>
2022-01-28 20:17:12 -0800

    set up initial storyboard controller w nav and tab cons

--- PetitionWhiteHouse/ViewController.swift
+++ PetitionWhiteHouse/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell    = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text    = "Title goes here"
+        cell.detailTextLabel?.text  = "Subtitle goes here"
         return cell
     }
 }
